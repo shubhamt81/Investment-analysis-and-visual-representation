b5e2008c8510a8d9c11144c24259fa2d0307d290
shubhamt81 <74250237+shubhamt81@users.noreply.github.com>
2023-05-06 23:30:15 +0530

    changes in server.py

--- frontend/flask-server/server.py
+++ frontend/flask-server/server.py
@@ -19,16 +19,18 @@ def funds():
     q="select * from returns;"
     cur=mysql.connection.cursor()
     cur.execute(q)
-    rz=cur.fetchone()
+    rz=cur.fetchall()
     cur.close()
-    s=[]
+    ret=[]
     q=""
-    print(rz)
-    for i in rz:
-        s.append(str(i))
-        q+=str(i)
-    print(s)
-    return {"Company_name":s[0],"name_of_plan":s[1],"cap_size":s[2],"type_of_investment":s[3],"ROI":s[4],"risk":s[5],"min_investment":s[6],"ROI_1M":s[7],"ROI_3M":s[8],"ROI_6M":s[9],"ROI_1Y":s[10],"ROI_3Y":s[11],"ROI_5Y":s[12],"ROI_10Y":s[13],"cat":s[14],"sub_cat":s[15],"vro_rating":s[16],"lumpsum_min":s[17],"sip_min":s[18],"aum":s[19],"nav":s[20],"CAT_AVG_10Y":s[21],"CAT_AVG_5Y":s[22],"CAT_AVG_3Y":s[23],"CAT_AVG_1Y":s[24],"CAT_AVG_6M":s[25],"CAT_AVG_3M":s[26],"CAT_AVG_1M":s[27]}
+    for j in rz:
+        s=[]
+        for i in j:
+            s.append(str(i))
+            q+=str(i)
+        ret.append({"Company_name":s[0],"name_of_plan":s[1],"cap_size":s[2],"type_of_investment":s[3],"ROI":s[4],"risk":s[5],"min_investment":s[6],"ROI_1M":s[7],"ROI_3M":s[8],"ROI_6M":s[9],"ROI_1Y":s[10],"ROI_3Y":s[11],"ROI_5Y":s[12],"ROI_10Y":s[13],"cat":s[14],"sub_cat":s[15],"vro_rating":s[16],"lumpsum_min":s[17],"sip_min":s[18],"aum":s[19],"nav":s[20],"CAT_AVG_10Y":s[21],"CAT_AVG_5Y":s[22],"CAT_AVG_3Y":s[23],"CAT_AVG_1Y":s[24],"CAT_AVG_6M":s[25],"CAT_AVG_3M":s[26],"CAT_AVG_1M":s[27]}) 
+        print(s)
+    return ret
     
     
 if __name__=="__main__":
